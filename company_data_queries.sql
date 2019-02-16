adf941fb99e72d02b074061a33e5505e1691d384
Tim <timhulak@gmail.com>
2019-02-15 21:24:06 -0800

    Working though the employee table

--- company_data_queries.sql
+++ company_data_queries.sql
@@ -3,17 +3,23 @@ USE employees;
 /*
 Boilerplate Query Structure
 SELECT 
-	column_name_1, column_name_2
+	column_name_1, column_name_2, etc
 FROM 
 	table_name
 WHERE
 	condition
-    (AND comes before OR and parentesis help with execution order)
+    (AND comes before OR) (and parentesis help 
+    with execution order)
+GROUP BY (when using aggregate functions)
+	column
+HAVING
+	condition with aggregate function
+ORDER BY
+	column
 ;
 
 */
 /*
-
 Wildcard Charachters:
 
 % : a substitute for a sequence of characters 
@@ -25,8 +31,20 @@ _ : match a single character
 
 */
 
-/* PART 1
-   Employee Information */
+/*
+Common Aggregate Functions:
+COUNT() counts non-null records in a columns
+SUM() adds all non-null values in a column
+MIN() returns minimum value 
+MAX() returns maximum value
+AVG() returns the average of all non-null values
+*/
+
+/* 
+PART 1
+Employee Information 
+*/
+
 -- Explore the "employees" table
 SELECT 
     *
@@ -177,9 +195,300 @@ FROM
 WHERE
     emp_no LIKE ('1000_');
 
+/*
+Extract all individuals from the ‘employees’ table whose 
+first name contains “Jack”.
+*/
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name LIKE ('%Jack%');
 
-/* PART 2
-   Department Information */
+/*
+Once you have done that, extract another list containing 
+the names of employees that do not contain “Jack”.
+*/
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name NOT LIKE ('%Jack%');
+
+/*
+Select all the information from the “salaries” table regarding 
+contracts from 66,000 to 70,000 dollars per year.
+*/
+SELECT
+    *
+FROM
+    salaries;
+    
+SELECT
+    *
+FROM
+    salaries
+WHERE
+    salary BETWEEN 66000 AND 70000;
+/*
+Retrieve a list with all individuals whose employee number is 
+not between ‘10004’ and ‘10012’.
+*/
+SELECT
+    *
+FROM
+    employees
+WHERE
+    emp_no NOT BETWEEN '10004' AND '10012';
+    
+/*
+Select the names of all departments with numbers between 
+‘d003’ and ‘d006’.
+*/
+SELECT
+    dept_name
+FROM
+    departments
+WHERE
+    dept_no BETWEEN 'd003' AND 'd006';
+
+-- Check to see if any records are missing;
+-- which employees do not have first names?
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name IS NULL;
+    
+-- Which employees were hired after January 1st, 2000?
+select * from employees;
+
+/*
+Retrieve a list with data about all female employees who were 
+hired in the year 2000 or after.
+*/
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date >=  '2000-01-01' AND gender = 'F';
+
+/*
+Extract a list with all employees’ salaries higher 
+than $150,000 per annum.
+*/
+
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    salary > 150000;
+
+/*
+Obtain a list with all different “hire dates”
+ from the “employees” table.
+*/
+SELECT DISTINCT
+    hire_date
+FROM
+    employees;
+
+/*
+How many employees are registered in our database?
+*/
+SELECT 
+    COUNT(emp_no)
+FROM
+    employees;
+    
+/*
+How many distinct names are in the employee table? 
+*/
+SELECT 
+    COUNT(DISTINCT first_name)
+FROM
+    employees;
+    
+/*
+How many annual contracts with a value higher than or 
+equal to $100,000 have been registered in the 
+salaries table?
+*/
+SELECT 
+    COUNT(*)
+FROM
+    salaries
+WHERE
+    salary >= 100000;
+
+/*
+How many managers do we have in the “employees” database? 
+Use the star symbol (*) in your code to solve this exercise.
+*/
+SELECT 
+    COUNT(*)
+FROM
+    dept_manager;
+
+/*
+Get all of the employee data from the 
+employees table and order it by first name
+
+Then order the names ascending followed by another 
+query ordering them descending
+*/
+SELECT 
+    *
+FROM
+    employees
+ORDER BY first_name;
+
+-- ascending
+SELECT 
+    *
+FROM
+    employees
+ORDER BY first_name ASC;
+
+-- descending
+SELECT 
+    *
+FROM
+    employees
+ORDER BY first_name DESC;
+
+/*
+Sort the list of employee data
+in descending order by employee number
+*/
+SELECT 
+    *
+FROM
+    employees
+ORDER BY emp_no DESC;
+
+/*
+Get the data from the employees table 
+and order it by both 
+first and last name 
+in ascending order
+*/
+SELECT 
+    *
+FROM
+    employees
+ORDER BY first_name , last_name ASC;
+
+/*
+Select all data from the “employees” table,
+ ordering it by “hire date” in descending order.
+*/
+SELECT 
+    *
+FROM
+    employees
+ORDER BY hire_date DESC;
+
+/*
+Count how many employees have the same first name.
+Make sure it is in alphabetical order from A-Z. 
+Don't forget to display the name next the count. 
+*/
+SELECT 
+    first_name, COUNT(first_name)
+FROM
+    employees
+GROUP BY first_name
+ORDER BY first_name ASC;
+
+/*
+Clean up to column names of that last query
+so that it can be presented to someone 
+more clearly; alias the column names to 
+something more clear
+*/
+SELECT 
+    first_name AS "First Name", COUNT(first_name) AS "Name Count"
+FROM
+    employees
+GROUP BY first_name
+ORDER BY first_name ASC;
+
+
+/*
+Write a query that obtains two columns. 
+The first column must contain annual salaries 
+higher than 80,000 dollars. 
+The second column, renamed to “Employees With Salary”, 
+must show the number of employees contracted 
+to that salary. Lastly, sort the output by 
+the first column.
+*/
+SELECT
+    salary AS "Salary", COUNT(emp_no) AS "Employees With Salary"
+FROM
+    salaries
+WHERE
+    salary > 80000
+GROUP BY salary
+ORDER BY salary;
+
+/*
+Select all employees whose average salary is 
+higher than $120,000 per annum.
+*/
+SELECT 
+    emp_no, AVG(salary)
+FROM
+    salaries
+GROUP BY emp_no
+HAVING AVG(salary) > 120000
+ORDER BY emp_no;
+
+/*
+
+*/
+
+/*
+
+*/
+
+/*
+
+*/
+
+
+/*
+
+*/
+
+
+/*
+
+*/
+
+
+/*
+
+*/
+
+
+/*
+
+*/
+
+
+-- --------------------------------------
+/* 
+PART 2
+Department Information 
+*/
+
 -- Explore the "departments" table
 SELECT 
     *
@@ -191,4 +500,15 @@ SELECT
     COUNT(dept_no) AS "Total Departments"
 FROM
     departments;
+
+/*
+Select the names of all departments whose department number 
+value is not null.
+*/
+SELECT 
+    dept_name
+FROM
+    departments
+WHERE
+    dept_no IS NOT NULL;
 
